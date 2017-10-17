e4381a5ee492971312715cf01f3fbe9a92e2e7f3
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-10-16 17:26:27 -0700

    Working on bounding the IDW by strip that the crops occur

--- Input/readme.txt
+++ Input/readme.txt
@@ -3,3 +3,7 @@
 	* Derived from HY2013GP_171010.xlsx
 * [Polylines]
 	* Inported from CafModelingMetricCropSyst\Working\ArcMap\Working
+
+## 10/16/2017
+* [CookEastStrips]
+	* From: G:\My Drive\Backup\FromNasRm243_160615_oldDataBackup\DataDriveBackup\Projects\Back-Up\SD Cards 122712\1\Grid_Sampling
--- exploring2013.R
+++ exploring2013.R
@@ -6,9 +6,8 @@ library(gstat)
 #' Creates a IDW map of yield values from grain weight
 #' @param data.path A string of the path to csv file with grain mass data in WGS84 datum and columns: FieldID	Column	Row2	ID2	Latitude	Longitude	Year	SampleID	Crop	GrainWeightWet
 #' @param area.harvested A number for the area harvested in m2, grain mass will be divided by this number to get mass/area
-#' @param polygon.shpdir A string for the relative folder containing a polygon ESRI shapefile
-#' @param polygon.shpname A string for the name, without extension, of the polygon ESRI shapefile
-map_yield <- function(data.path, area.harvested, polygon.shpdir, polygon.shpname, crop.name = "") {
+#' @param polygon A SpatialPolygonsDataFrame of the boundary to be analyzed
+map_yield <- function(data.path, area.harvested, boundary, crop.name = "") {
   # Read data and clean nulls
   d <- read.csv(data.path, stringsAsFactors = TRUE)
   d <- d[!(is.na(d$GrainWeightWet) | d$GrainWeightWet==""), ]
@@ -21,10 +20,10 @@ map_yield <- function(data.path, area.harvested, polygon.shpdir, polygon.shpname
   d['Yield'] <- d$GrainWeightWet / area.harvested
   
   # Print shapefile info
-  ogrInfo(polygon.shpdir, polygon.shpname)
+  #ogrInfo(polygon.shpdir, polygon.shpname)
   
   # Read boundary shapefile
-  boundary <- readOGR(polygon.shpdir, polygon.shpname)
+  #boundary <- readOGR(polygon.shpdir, polygon.shpname)
   
   # Convert csv gain mass data to spatial points and specify the datum
   dsp <- SpatialPoints(d[,6:5], proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs"))
@@ -41,7 +40,7 @@ map_yield <- function(data.path, area.harvested, polygon.shpdir, polygon.shpname
   # Confine to given boundary
   ca <- aggregate(boundaryta)
   vca <- intersect(v, ca)
-
+  
   # Rasterize it
   r <- raster(boundaryta, res=5)
   vr <- rasterize(vca, r, 'Yield')
@@ -53,7 +52,7 @@ map_yield <- function(data.path, area.harvested, polygon.shpdir, polygon.shpname
   plot(idwr, col = grey.colors(n = 8, 0, 1))
   #, ColorRampPalette=colorRampPalette("white", "black")
   # Plot points on top of IDW raster
-
+  
   
   points(dta, col=c("white", "chartreuse", "blue", "black", "red", "green", "sandybrown")[dta$Crop])
   
@@ -63,61 +62,100 @@ map_yield <- function(data.path, area.harvested, polygon.shpdir, polygon.shpname
   mtext(paste("Area harvested = ", toString(area.harvested)))
 }
 
-map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384, "Input/CookEastArea", "CafCookEastArea")
+#' Creates a IDW map of yield values from grain weight
+#' @param data.path A string of the path to csv file with grain mass data in WGS84 datum and columns: FieldID	Column	Row2	ID2	Latitude	Longitude	Year	SampleID	Crop	GrainWeightWet
+#' @param area.harvested A number for the area harvested in m2, grain mass will be divided by this number to get mass/area
+#' @param polygon.shpdir A string for the relative folder containing a polygon ESRI shapefile
+#' @param polygon.shpname A string for the name, without extension, of the polygon ESRI shapefile
+map_yield_path <- function(data.path, area.harvested, polygon.shpdir, polygon.shpname, crop.name = "") {
+  # Print shapefile info
+  ogrInfo(polygon.shpdir, polygon.shpname)
+  
+  # Read boundary shapefile
+  boundary <- readOGR(polygon.shpdir, polygon.shpname)
+  
+  map_yield(data.path, area.harvested, boundary)
+}
+
+extract_georef_field_and_strip <- function(a.strips, b.strips, c.strips, polygons) {
+  #b.fields <- polygons[polygons$Field %in% fields, ]
+  #b.strips <- b.fields[b.fields$Strip %in% strips, ]
+  strips <- polygons[
+      (polygons$Field == "A" & polygons$Strip %in% a.strips) |
+      (polygons$Field == "B" & polygons$Strip %in% b.strips) |
+      (polygons$Field == "C" & polygons$Strip %in% c.strips), ]
+}
+
+setwd("C:\\OneDrive\\OneDrive - Washington State University (email.wsu.edu)\\Projects\\CookEastPlantHandHarvest\\1999-2016\\Working\\R")
+
+b <- readOGR("Working/CookEastStrips2013", "Field_Plan_Final")
+boundary <- readOGR("Input/CookEastArea", "CafCookEastArea")
+
+georeff.only <- raster::intersect(boundary, b)
+
+#GB
+a.strips <- c(5)
+b.strips <- c(1,2,3,4)
+c.strips <- NULL
+map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384,extract_georef_field_and_strip(a.strips, b.strips, c.strips, georeff.only), "GB")
+
+#SB
+a.strips <- NULL
+b.strips <- C(5)
+c.strips <- NULL
+map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384,extract_georef_field_and_strip(a.strips, b.strips, c.strips, georeff.only), "SB")
+
+#SW
+a.strips <- c(2,3,4)
+b.strips <- c(5, 6)
+c.strips <- NULL
+map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384,extract_georef_field_and_strip(a.strips, b.strips, c.strips, georeff.only), "SW")
+
+#WW
+a.strips <- c(1)
+b.strips <- NULL
+c.strips <- c(1, 2, 3, 4, 5, 6, 7, 7, 8)
+map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384,extract_georef_field_and_strip(a.strips, b.strips, c.strips, georeff.only), "WW")
+
+
+
+
+map_yield_path("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384, "Input/CookEastArea", "CafCookEastArea")
+
 
 ## ============= PLAYGROUND =================
-#d <- read.csv("Input/HY2013GP_GrainWeightOnly_171012.csv")
-## clean nulls
-#d <- d[!(is.na(d$GrainWeightWet) | d$GrainWeightWet==""), ]
-#d['WetYield1.828m2'] <- d$GrainWeightWet / 1.828
-#d['WetYield2.4384m2'] <- d$GrainWeightWet / 2.4384
-#
-## --- from: https://www.nceas.ucsb.edu/scicomp/usecases/ReadWriteESRIShapeFiles ---
-#
-## optionally report shapefile details
-#ogrInfo("Input/Polylines", "CookEastBoundary")
-#ogrInfo("Input/CookEastArea", "CafCookEastArea")
-#
-## read in shapefiles
-##CookEastBoundary <- readOGR("Input/Polylines", "CookEastBoundary")
-#CookEastBoundary <- readOGR("Input/CookEastArea", "CafCookEastArea")
-#
-## note that readOGR will read the .prj file if it exists
-#print(proj4string(CookEastBoundary))
-#
-## generate map
-#plot(CookEastBoundary, axes=TRUE, border="gray")
-## -----------------
-#
-## --- from: http://rspatial.org/analysis/rst/4-interpolation.html ---
-#
-#dsp <- SpatialPoints(d[,6:5], proj4string=CRS("+proj=longlat +datum=WGS84 +no_defs"))
-#dsp <- SpatialPointsDataFrame(dsp, d)
-#
-#cuts <- c(0,200,400,600,800,100,1200,1400)
-#blues <- colorRampPalette(c('yellow', 'orange', 'blue', 'dark blue'))
-#pols <- list("sp.polygons", CookEastBoundary)
-#spplot(dsp, 'GrainWeightWet', cuts=cuts, col.regions=blues(5), sp.layout=pols, pch=20, cex=2)
-#
-#TA <- CRS("+proj=utm +zone=11 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
-#dta <- spTransform(dsp, TA)
-#cata <- spTransform(CookEastBoundary, TA)
-#
-#plot(cata)
-#v <- voronoi(dta)
-#plot(v)
-#
-#ca <- aggregate(cata)
-#vca <- intersect(v, ca)
-#spplot(vca, 'GrainWeightWet', col.regions=rev(get_col_regions()))
-#
-#r <- raster(CookEastBoundary, res=5)
-#vr <- rasterize(vca, r, 'GrainWeightWet')
-#plot(vr)
-#
-#gs <- gstat(formula = GrainWeightWet~1, locations=dta)
-#idw <- interpolate(r, gs)
-#idwr <- mask(idw, vr)
-#plot(idwr)
-#
-## -------------------------
+## --- Loading polygons in shp file ---
+library(sp)
+library(rgdal)
+setwd("C:\\OneDrive\\OneDrive - Washington State University (email.wsu.edu)\\Projects\\CookEastPlantHandHarvest\\1999-2016\\Working\\R")
+ogrInfo("Working/CookEastStrips2013", "Field_Plan_Final")
+b <- readOGR("Working/CookEastStrips2013", "Field_Plan_Final")
+boundary <- readOGR("Input/CookEastArea", "CafCookEastArea")
+georeff.only <- raster::intersect(boundary, b)
+georeff.A.only <- subset(georeff.only, Field == "A")
+plot(georeff.A.only)
+map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384, georeff.A.only, crop.name = "SW")
+nb <- subset(b, Crop == "SB")
+fields <- c("A", "B")
+strips <- c(1, 2)
+b.fields <- georeff.only[georeff.only$Field %in% fields, ]
+b.strips <- b.fields[b.fields$Strip %in% strips, ]
+fields.strips <- matrix(FALSE, nrow=8, ncol=3)
+plot(extract_georef_field_and_strip(c("A", "C"), c(1, 2, 3, 4, 5, 6, 7, 8), georeff.only))
+
+a.strips <- c(1)
+b.strips <- NULL
+c.strips <- c(1, 2, 3, 4, 5, 6, 7, 7, 8)
+
+b.strips.a <- georeff.only[
+  (georeff.only$Field == "A" & georeff.only$Strip %in% a.strips) |
+  (georeff.only$Field == "B" & georeff.only$Strip %in% b.strips) |
+  (georeff.only$Field == "C" & georeff.only$Strip %in% c.strips), ]
+
+plot(extract_georef_field_and_strip(a.strips, b.strips, c.strips, georeff.only))
+
+map_yield("Input/HY2013GP_GrainWeightOnly_171012.csv", 2.4384,extract_georef_field_and_strip(a.strips, b.strips, c.strips, georeff.only), "WW")
+
+plot(b.fields)
+plot(b.strips)
+plot(georeff.only)
