ec87c6bc0815a38451a9d19d76c4afb1b8484327
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-11-29 09:01:46 -0800

    Created a test output file to compare selection based on script with manual selection of values -- a poor mans unit test

--- cleaningHY2014/GPHY2017selectBestData.R
+++ cleaningHY2014/GPHY2017selectBestData.R
@@ -8,7 +8,112 @@ library(geojsonio)
 #library(jsonlite)
 #library(spatialEco)
 
-y <- read.csv("Input/HY2014GB_aggregate_all_data_171122.csv")
-georef <- geojson_read("Input/CookEast_GeoreferencePoints_171127.json", what = "sp")
+setwd("C:/Dev/Projects/CookEastPlantHandHarvest/R/cleaningHY2014")
 
-t <- merge(y, georef, by.x = c("Col", "Row2"), by.y = c("Column", "Row2"), all.x = TRUE)
+hy2014 <- read.csv("Input/HY2014GB_aggregate_all_data_171122.csv")
+hy2014$ShoudKeep <- NULL
+georef <- geojson_read("Input/CookEast_GeoreferencePoints_171127.json",
+                       what = "sp")
+# Merge the data based on col and row2 because John Morse said he focused on
+# row and columns, not ID2 values (this is important because ID2 values are not
+# consistent with row/column values)
+df <- merge(hy2014, georef,
+           by.x = c("Col", "Row2"),
+           by.y = c("Column", "Row2"),
+           all.x = TRUE)
+
+# Remove missing data
+dfc <- df[!is.na(df$ID2.y),]
+
+# Create dataframe with "good" values
+dfg <- subset(dfc, FALSE)
+
+for (id in 1:max(dfc$ID2.y, na.rm = TRUE)) {
+    reps <- dfc[dfc$ID2.y == id,]
+
+    # Keep if is only value
+    if (nrow(reps) == 1) {
+        dfg <- rbind(dfg, reps)
+        next
+    }
+
+    # Keep if NIR data, next iteration if found
+    rep.withProtein <- reps[which(!is.na(reps$protein)),]
+    if (nrow(rep.withProtein) == 1) {
+        dfg <- rbind(dfg, rep.withProtein)
+        next
+    }
+    else if (nrow(rep.withProtein) > 1) {
+        # If all rows are duplicates, then keep first row
+        if (nrow(rep.withProtein[duplicated(rep.withProtein), ]) == 
+            (nrow(rep.withProtein) / 2)) {
+              dfg <- rbind(dfg, rep.withProtein[1,])
+              next
+        }
+        else if (length(unique(rep.withProtein$TotalGrain.g.))==1) {
+            # If grain mass are all the same, just take first row
+            dfg <- rbind(dfg, rep.withProtein[1,])
+            next
+        }
+        else {
+            stop(paste("id:", id, "- something wrong with protein"))
+        }
+    }
+
+    # Keep if weighed in 2015, next iteration if so
+    rep.weighed2015 <- reps[
+        which(
+            as.numeric(format(
+                as.Date(reps$LastSaved, origin = '1900-1-1'), "%Y")) >= 2015),]
+    if (nrow(rep.weighed2015) == 1) {
+        dfg <- rbind(dfg, rep.weighed2015)
+        next
+    }
+    else if (nrow(rep.weighed2015) > 1) {
+        # If all rows are duplicates, then keep first row
+        if (nrow(rep.weighed2015[duplicated(rep.weighed2015), ]) ==
+            (nrow(rep.weighed2015) / 2)) {
+            dfg <- rbind(dfg, rep.weighed2015[1,])
+            next
+        }
+        else if (length(unique(rep.weighed2015$TotalGrain.g.)) == 1) {
+            # If grain mass are all the same, just take first row
+            dfg <- rbind(dfg, rep.weighed2015[1,])
+            next
+        }
+        else {
+            stop(paste("id:", id, "something wrong checking weighed in 2015"))
+        }
+    }
+
+    # Keep if has grain+bag value
+    rep.withGrainBagWeight <- reps[which(!is.na(reps$TareBag.g..1)),]
+    if (nrow(rep.withGrainBagWeight) == 1) {
+        dfg <- rbind(dfg, rep.withGrainBagWeight)
+        next
+    }
+    else if (nrow(rep.withGrainBagWeight) > 0) {
+        # If all rows are duplicates, then keep first row
+        if (nrow(rep.withGrainBagWeight[duplicated(rep.withGrainBagWeight), ]) ==
+            (nrow(rep.withGrainBagWeight) / 2)) {
+                dfg <- rbind(dfg, rep.withGrainBagWeight[1,])
+                next
+        }
+        else if (length(unique(rep.withGrainBagWeight$TotalGrain.g.)) == 1) {
+            # If grain mass are all the same, just take first row
+            dfg <- rbind(dfg, rep.withGrainBagWeight[1,])
+            next
+        }
+        else {
+            stop(paste("id:", id, "something wrong with grain+bag weight"))
+        }
+        
+    }
+}
+
+filePath <- paste(
+                  "Output/selectValueTest_",
+                  format(Sys.Date(), "%y%m%d"),
+                  ".csv",
+                  sep = "")
+write.csv(dfg, filePath)
--- cleaningHY2014/Input/readme.txt
+++ cleaningHY2014/Input/readme.txt
@@ -5,3 +5,7 @@
 	* From CosmsoDB, document id = filename (without ".json")
 * [CAF_strips]
 	* From "CAF_strips" in: https://data.nal.usda.gov/dataset/data-field-scale-sensor-network-data-set-monitoring-and-modeling-spatial-and-temporal-variation-soil-moisture-dryland-agricultural-field
+
+## 11/29/2017
+* [HY2014GB_aggregate_all_data_manualSelection_171129.csv]
+	* Modification of [HY2014GB_aggregate_all_data_171122.csv].  Manually deleted values that were not to be kept -- or rows with 0 value in "ShouldKeep" column
